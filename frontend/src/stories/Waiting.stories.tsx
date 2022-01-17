d07eb222c021f37669bd003ba3acd3645a190afb
Varun Iyer <varun_iyer@posteo.net>
2022-01-16 18:47:12 -0800

    Added Waiting Alert popover

---
+++ frontend/src/stories/Waiting.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Waiting, {WaitingProps} from '../Waiting';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Words/Waiting',
+  component: Waiting,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof Waiting>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Waiting> = (args) => <Waiting {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  bodyText: 'Game ID testy-fraud',
+}
