f291977797e092397f9263bb5b7f7db08bf5065e
Faiz Surani <faiz.surani@gmail.com>
2022-01-17 21:27:05 -0800

    Fix api url env variable

--- frontend/src/services/requester.ts
+++ frontend/src/services/requester.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = {
-  baseURL: process.env.BATTLEWORD_API_URL || 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_BATTLEWORD_API_URL || 'http://localhost:5000',
 };
 
 export default axios.create(axiosConfig);
