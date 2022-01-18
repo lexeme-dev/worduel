cdffbfa0a46513975c308ba549fcf01712d5fae4
Faiz Surani <faiz.surani@gmail.com>
2022-01-18 01:01:16 -0800

    Add keyboard entry support to GuessKeyboard

--- frontend/src/components/GuessKeyboard.tsx
+++ frontend/src/components/GuessKeyboard.tsx
@@ -30,67 +30,99 @@ const KEYBOARD_LAYOUT = [
   ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACKSPACE']
 ];
 
-function GuessKeyboard(props: GuessKeyboardProps) {
-
-  const onBackspace = () => {
-    const newWord = props.currentWord.slice(0, -1);
-    props.onChange(newWord);
+class GuessKeyboard extends React.Component<GuessKeyboardProps> {
+  componentWillMount() {
+    window.addEventListener('keyup', this.handleKeyInput);
   }
 
-  const onKeyPress = (key: string) => {
-    const newWord = props.currentWord + key.toLowerCase();
-    props.onChange(newWord);
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyInput);
   }
 
-  const disabled = !props.enabled || props.currentWord.length >= 5;
-  return (
-    <div className="guess-keyboard pt-3">
-      <Container>
-        {KEYBOARD_LAYOUT.map((keys, i) =>
-          <Row className="justify-content-md-center" key={i}>
-            {keys.map((key, j) => {
-                if (key === "ENTER") {
-                  return (
-                    <Col className="enter-key" key={j}>
-                      <Button onClick={() => props.onSubmit()}
-                              size="sm"
-                              className={!props.enabled || props.currentWord.length < 5 ? "key-disabled" : ""}
-                      >
-                        GUESS
-                      </Button>
-                    </Col>)
-                } else if (key === "BACKSPACE") {
-                  return (
-                    <Col className="enter-key" key={j}>
-                      <Button
-                        size="sm"
-                        onClick={() => onBackspace()}
-                        className={!props.enabled || props.currentWord.length === 0 ? "key-disabled" : ""}
-                      >
-                        <BackspaceFill/>
-                      </Button>
-                    </Col>)
-                } else {
-                  return (
-                    <Col className="key-col" key={j}>
-                      <Button
-                        className={`${stateVariantMap[props.knowledge[key.toLowerCase()]]} ${disabled ? "key-disabled" : ""}`}
-                        onClick={() => onKeyPress(key)}
-                        size="sm">
-                        {key}
-                      </Button>
-                    </Col>
-                  );
+  handleKeyInput = (event: KeyboardEvent) => {
+    if (event.key === "Enter" && !this.enterDisabled()) {
+      this.props.onSubmit();
+    } else if (event.key === "Backspace" && !this.backspaceDisabled()) {
+      this.onBackspace();
+    } else if (/^[a-zA-z]$/.test(event.key) && !this.lettersDisabled()) {
+      this.onKeyPress(event.key);
+    }
+  }
+
+  onBackspace = () => {
+    const newWord = this.props.currentWord.slice(0, -1);
+    this.props.onChange(newWord);
+  }
+
+  onKeyPress = (key: string) => {
+    const newWord = this.props.currentWord + key.toLowerCase();
+    this.props.onChange(newWord);
+  }
+
+  private backspaceDisabled() {
+    return !this.props.enabled || this.props.currentWord.length === 0;
+  }
+
+  private lettersDisabled() {
+    return !this.props.enabled || this.props.currentWord.length >= 5;
+  }
+
+  private enterDisabled() {
+    return !this.props.enabled || this.props.currentWord.length < 5;
+  }
+
+  render() {
+    const disabled = this.lettersDisabled();
+    return (
+      <div className="guess-keyboard pt-3">
+        <Container>
+          {KEYBOARD_LAYOUT.map((keys, i) =>
+            <Row className="justify-content-md-center" key={i}>
+              {keys.map((key, j) => {
+                  if (key === "ENTER") {
+                    return (
+                      <Col className="enter-key" key={j}>
+                        <Button onClick={() => this.props.onSubmit()}
+                                size="sm"
+                                className={this.enterDisabled() ? "key-disabled" : ""}
+                        >
+                          GUESS
+                        </Button>
+                      </Col>)
+                  } else if (key === "BACKSPACE") {
+                    return (
+                      <Col className="enter-key" key={j}>
+                        <Button
+                          size="sm"
+                          onClick={() => this.onBackspace()}
+                          className={this.backspaceDisabled() ? "key-disabled" : ""}
+                        >
+                          <BackspaceFill/>
+                        </Button>
+                      </Col>)
+                  } else {
+                    return (
+                      <Col className="key-col" key={j}>
+                        <Button
+                          className={`${stateVariantMap[this.props.knowledge[key.toLowerCase()]]} ${disabled ? "key-disabled" : ""}`}
+                          onClick={() => this.onKeyPress(key)}
+                          size="sm">
+                          {key}
+                        </Button>
+                      </Col>
+                    );
+                  }
                 }
-              }
-            )}
+              )}
+            </Row>
+          )}
+          <Row>
           </Row>
-        )}
-        <Row>
-        </Row>
-      </Container>
-    </div>
-  );
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default GuessKeyboard;
