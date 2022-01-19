cb2b54dd0543941bf676bb627c96fcb4b867a1e8
Faiz Surani <faiz.surani@gmail.com>
2022-01-19 01:38:57 -0800

    Add cordova config files (working POC)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,14 +5,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const startApp = () => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+    startApp();
+}
